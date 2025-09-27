9eeda0b0ebdba56daa993f1f4fb06ddaebe93caf
AlanFromJapan <alanfromjapan@gmail.com>
2025-09-27 21:52:36 +0900

    Now with bg thread

--- plugins/plugin_wol.py
+++ plugins/plugin_wol.py
@@ -2,11 +2,14 @@ from gi.repository import Gtk
 import socket
 import os
 from dotenv import load_dotenv
+import threading
+import time
 
 #load environment variables from a .env file
 load_dotenv()
 
 __indicator = None
+__thread = None
 
 #The register function is required for the plugin system to recognize this file as a plugin.
 def register(menu, indicator):
@@ -20,20 +23,28 @@ def register(menu, indicator):
 
 
 def send_wol(_):
-    print("Sending WOL packet...")
+    print("Starting WOL sending thread...")
+    __thread = threading.Thread(target=background_task, daemon=True)
+    __thread.start()
 
-    # Replace with your NAS MAC address
-    mac_address = os.getenv("WOL") or "00:11:22:33:44:55"
 
-    print(f"Sending WOL packet to {mac_address}...")
+def background_task():
+    while True:
+        print("Sending WOL packet...")
 
-    # Build the magic packet
-    mac_bytes = bytes.fromhex(mac_address.replace(":", ""))
-    magic_packet = b'\xff' * 6 + mac_bytes * 16
+        # Replace with your NAS MAC address
+        mac_address = os.getenv("WOL") or "00:11:22:33:44:55"
 
-    # Send the packet to the broadcast address on UDP port 9
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.sendto(magic_packet, ('<broadcast>', 9))
+        # Build the magic packet
+        mac_bytes = bytes.fromhex(mac_address.replace(":", ""))
+        magic_packet = b'\xff' * 6 + mac_bytes * 16
 
-    print("WOL packet sent to", mac_address)
+        # Send the packet to the broadcast address on UDP port 9
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            sock.sendto(magic_packet, ('<broadcast>', 9))
+
+        print("WOL packet sent to", mac_address)
+
+        time.sleep(3 * 60)  # Wait for 3 minutes before sending again
+
