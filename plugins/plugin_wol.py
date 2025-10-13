2caf39be800077fb1efc695ad3dc5189f05051fd
AlanFromJapan <alanfromjapan@gmail.com>
2025-10-13 22:02:40 +0900

    Added logs to the plugins

--- plugins/plugin_wol.py
+++ plugins/plugin_wol.py
@@ -15,7 +15,7 @@ def register(menu, indicator):
     global __indicator
     global __menu_item
     __indicator = indicator
-    print("Plugin 'plugin_wol' registered")
+    shared.logger.info("Plugin 'plugin_wol' registered")
 
     __menu_item = Gtk.CheckMenuItem(label="Wake-On-LAN (WOL)")
     __menu_item.connect("activate", send_wol)
@@ -39,12 +39,12 @@ def send_wol(_, autostart=False):
         if autostart or __menu_item.get_active():
             __menu_item.set_active(True) # Keep it checked to show it's active
 
-            print("Starting WOL sending thread...")
+            shared.logger.info("Starting WOL sending thread...")
             __thread_kill = False
             __thread = threading.Thread(target=background_task, daemon=True)
             __thread.start()
         else:
-            print("Stopping WOL sending thread...")
+            shared.logger.info("Stopping WOL sending thread...")
             __menu_item.set_active(False) # Keep it unchecked to show it's inactive
             __thread_kill = True
             # this will stop after the next sleep cycle in background_task, hoping user don't restart it before then
@@ -58,7 +58,7 @@ def background_task():
             mac_address = device.get('mac', '')
             if mac_address != '':
                 try:
-                    print(f"Preparing to send WOL packet to '{device.get('name', '<unknown>')}' [{mac_address}] ...")
+                    shared.logger.debug(f"Preparing to send WOL packet to '{device.get('name', '<unknown>')}' [{mac_address}] ...")
                     # Build the magic packet
                     mac_bytes = bytes.fromhex(mac_address.replace(":", ""))
                     magic_packet = b'\xff' * 6 + mac_bytes * 16
@@ -68,10 +68,10 @@ def background_task():
                         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
                         sock.sendto(magic_packet, ('<broadcast>', 9))
 
-                    print(f"WOL packet sent to '{device.get('name', '<unknown>')}' [{mac_address}].")
+                    shared.logger.info(f"WOL packet sent to '{device.get('name', '<unknown>')}' [{mac_address}].")
                 except Exception as e:
-                    print(f"Error sending WOL packet to {mac_address}: {e}")
+                    shared.logger.error(f"Error sending WOL packet to {mac_address}: {e}")
 
         frequency_sec = int(config.get('settings', {}).get('frequency_sec', 180))
         time.sleep(frequency_sec)
-    print("🪦 WOL sending thread exiting...")
+    shared.logger.info("🪦 WOL sending thread exiting...")
